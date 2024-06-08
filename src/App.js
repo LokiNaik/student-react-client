f2b1b7a988682af2ceef0abaf7cf86b361b0ee6d
Lokesh <gugulothlokesh1998@gmail.com>
2024-06-08 11:13:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { Component, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 
 function App() {
+
+  const [name, setName] = useState("")
+  
+  const handleSubmit = () => {
+    console.log('button clicked!')
+    console.log(name)
+  }
+
+  function nameHandle(event) {
+    let nameObject = event.target.value
+    setName(nameObject)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello world</h1>
+      <div>
+        <input type='text' placeholder='Name' onInput={nameHandle}></input>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
+
   );
 }
 
